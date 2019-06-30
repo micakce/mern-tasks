08db98693d0ce0d76ae99af82e4b4700d8c6090f
micakce <micakce@gmail.com>
2019-06-30 20:03:35 -0300

    Finished firts part of tut (Backend)

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -8,12 +8,31 @@ router.get('/', async (req, res) => {
     res.json(tasks)
 });
 
+
+router.get('/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    res.json(task)
+});
+
 router.post('/', async (req, res) => {
     const { title, description } = req.body;
     const task = new Task({ title, description });
     await task.save();
     res.json({status: 'Task Saved'})
 });
+
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    const newTask = {title, description};
+    await Task.findByIdAndUpdate(req.params.id, newTask);
+    res.json({status: 'Task Updated'});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status: 'Task deleted'});
+});
+
 
 // router.get('/', (req, res) => {
 //     Task.find(function (err, tasks)  {
